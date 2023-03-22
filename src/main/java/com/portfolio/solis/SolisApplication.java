f04674aa56840585feb2837f0ca83689def755ce
arielSolis <108560263+ArielS76@users.noreply.github.com>
2023-03-22 10:21:56 -0300

    Add files via upload

---
+++ src/main/java/com/portfolio/solis/SolisApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.solis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolisApplication.class, args);
+	}
+
+}
